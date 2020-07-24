00fec45d1a03dee8f58432a17b362f8dffff4aaf
Tyler Burch <burcht11@gmail.com>
2020-07-24 11:09:19 -0500

    Add baseballR calling script

---
+++ python/fg_download.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+library(baseballr)
+
+if (length(args) == 1){
+    print(paste("Downloading data from",year))
+    leaderboards = fg_bat_leaders(x=year, y=year, league="all", qual="y")
+} else{
+    # Default to 2018
+    leaderboards = fg_bat_leaders(x=2018,y=2018, league="all", qual="y")
+}
+write.csv(leaderboards, "../data/leaderboards.csv")

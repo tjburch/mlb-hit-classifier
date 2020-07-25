e1afc1b88c727174b626f1f7e579d6f903871037
Tyler Burch <burcht11@gmail.com>
2020-07-25 14:46:28 -0500

    Fix file saving string for qualified

--- python/fg_download.R
+++ python/fg_download.R
@@ -3,19 +3,20 @@ args = commandArgs(trailingOnly=TRUE)
 
 library(baseballr)
 
-if (length(args) == 2){
-        # Just pass some arb second arg for unqualified
-        year = as.integer(args[1])
-        leaderboards = fg_bat_leaders(x=year, y=year, league="all", qual="n")
-        write.csv(leaderboards, paste("../data/uq_leaderboards",year,".csv"))
-    } else if (length(args) == 1) {
-        # Main functionality, pass year
-        year = as.integer(args)
-        print(paste("Downloading data from",year))
-        leaderboards = fg_bat_leaders(x=year, y=year, league="all", qual="y")
-        write.csv(leaderboards, paste("../data/leaderboards",year,".csv"))
-    } else {
-        # Default to 2018
-        leaderboards = fg_bat_leaders(x=2018,y=2018, league="all", qual="y")
-        write.csv(leaderboards, "../data/leaderboards2018.csv")
+if (length(args) == 2) {
+    # Just pass some arb second arg for unqualified
+    year = as.integer(args[1])
+    leaderboards = fg_bat_leaders(x=year, y=year, league="all", qual="n")
+    write.csv(leaderboards, paste("../data/uq_leaderboards_",year,".csv",sep=""))
+    print("Done!")
+} else if (length(args) == 1) {
+    # Main functionality, pass year
+    year = as.integer(args)
+    print(paste("Downloading data from",year))
+    leaderboards = fg_bat_leaders(x=year, y=year, league="all", qual="y")
+    write.csv(leaderboards, paste("../data/leaderboards_",year,".csv",sep=""))
+ } else {
+    # Default to 2018
+    leaderboards = fg_bat_leaders(x=2018,y=2018, league="all", qual="y")
+    write.csv(leaderboards, "../data/leaderboards2018.csv")
 }
